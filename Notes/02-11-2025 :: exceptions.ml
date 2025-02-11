9485637b6be95f6f75de0e4f3daa90e7065b50f2
Valeria Gomez <96849988+valeriagommez@users.noreply.github.com>
2025-02-11 15:22:28 -0500

    Update 02-11-2025 :: exceptions.ml

--- Notes/02-11-2025 :: exceptions.ml
+++ Notes/02-11-2025 :: exceptions.ml
@@ -10,16 +10,71 @@ let product_tr l return_outside =
     
 exception SawZero (* capital letter ==> it's a constructor! *)
                   
-let product_tr l = 
+let product l = 
   let rec go l = 
     match l with 
     | [] -> 1
     | x :: xs ->
-        if x = 0 then raise SawZeroo
+        if x = 0 then raise SawZero
         else x * go xs
   in go l
             
             
-(* raise : exn (exception) -> 'a 
-    - it returns ANYTGHING, however, it doesn't actually return ever 
+(* 
+
+raise : exn (exception) -> 'a 
+  - it returns ANYTGHING, however, it never actually returns 
+
+product [2;4;8;0] ;; ==> Exception: SawZero.
+  - However, we want it to just return 0!
+   
 *)
+
+let product l = 
+  let rec go l = 
+    match l with 
+    | [] -> 1
+    | x :: xs ->
+        if x = 0 then raise SawZero
+        else x * go xs
+  in 
+  try go l with
+  | SawZero -> 0
+  
+    
+(* 
+
+try : 
+    return go l
+else SawZero: 
+    return 0
+   
+*)
+
+type 'a tree = Empty | Node of 'a tree * 'a * 'a tree
+                   
+exception NotFound 
+    
+(* Finding the first element satisfying p : *)
+let rec find (p : 'a -> bool) (t : 'a tree) : 'a =
+  match t with 
+  | Empty -> raise NotFound
+  | Node (l, x, r) ->
+      if p x then x
+      else 
+        try find p l with   (* searching through left subtree *)
+        | NotFound -> find p r (* if that fails, then searching through right subtree *)
+                              
+          
+
+
+
+
+
+
+
+
+
+
+
+
