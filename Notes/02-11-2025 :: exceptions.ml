03a5bbbd6c20debb2bd01cf418f88e7b313586cf
Valeria Gomez <96849988+valeriagommez@users.noreply.github.com>
2025-02-11 15:52:18 -0500

    Update 02-11-2025 :: exceptions.ml

--- Notes/02-11-2025 :: exceptions.ml
+++ Notes/02-11-2025 :: exceptions.ml
@@ -64,10 +64,57 @@ let rec find (p : 'a -> bool) (t : 'a tree) : 'a =
       else 
         try find p l with   (* searching through left subtree *)
         | NotFound -> find p r (* if that fails, then searching through right subtree *)
-                              
-          
+  
+                        
+(* 
 
+CHANGE MAKING PROBLEM 
 
+- Try to find a selection of coins to add up to a certain target amount 
+
+Pathological currency : 
+- 7 cents ; 2 cents
+- Making change for 15 cents
+   - Greedy algorithm does NOT work !
+   - 15 - 2*7 = 1 ==> problem !
+
+- We're going to choose the biggest coin we can every time UNLESS we encounter
+  a problem, then we go back in time and Change our choice
+  
+*)
+
+exception Change
+
+let rec change (coins : int list) (amt : int) : int list = 
+  match coins, amt with
+  | _, 0 -> []
+  | [], _ -> raise Change (* we need to backtrack! *)
+  | c :: cs, amt ->
+      if c > amt then change cs amt 
+      else 
+        try c :: change (c::cs) (amt - c) with (* we can use a coin multiple times *)
+        | Change -> change cs amt (* if using c was bad, then we skip it *)
+                                  
+                                  
+let patho = [7;2]
+let canada = [100;25;10;5;1]
+             
+  
+(* Trying it out ! (WITHOUT EXCEPTIONS) *)
+
+let rec change (coins : int list) (amt : int) : int list options = 
+  failwith "use options" 
+
+let rec change (coins : int list) (amt : int) 
+    (succeed : int list -> 'r) (fail : unit -> 'r) : 'r =
+  match coins, amt with 
+  | _, 0 -> succeed []
+  | [], _ -> fail ()
+  | c :: cs, amt -> 
+      if c > amt then change cs amt succeed fail
+      else c :: change (c::cs) (amt - c) 
+             (fun solution -> succeed (c::solution))
+             (fun () -> change cs amt succeed failure)
 
 
 
